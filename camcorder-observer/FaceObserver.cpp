d5ffde81cd9f75d8924149884674f20e21630035
ffk1x <sxaxq7@gmail.com>
2024-05-15 23:46:18 +0300

    FaceObserver upd.

--- camcorder-observer/FaceObserver.cpp
+++ camcorder-observer/FaceObserver.cpp
@@ -1,12 +1,48 @@
 #include "FaceObserver.hpp"
 
+FaceObserver::FaceObserver(tg_bot::TelegramBot& bot) : tg_bot_ref(bot)
+{
+	faceCascade.load("haarcascade_frontalface_default.xml");
+	cap.open(0);
+}
 
 void FaceObserver::detectAndSaveFace()
 {
+	cv::Mat frame;
+	cap >> frame;
 
+	cv::Mat gray;
+	cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
+
+	std::vector<cv::Rect> faces;
+	faceCascade.detectMultiScale(gray, faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++) 
+	{
+		cv::Point center(faces[i].x + faces[i].width / 2,
+			faces[i].y + faces[i].height / 2);
+		cv::ellipse(frame, center, cv::Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, 
+			cv::Scalar(255, 0, 255), 4);
+
+		cv::Mat face_roi = frame(faces[i]);
+		cv::imwrite("detected_face.png", face_roi);
+		std::cout << "Face detected and saved to 'detected_face.png'" << std::endl;
+	}
+	cv::imshow("Face Detection", frame);
 }
 
 void FaceObserver::run()
 {
+	cv::namedWindow("Face Detection", cv::WINDOW_AUTOSIZE);
 
+	while (true)
+	{
+		detectAndSaveFace();
+
+		if (cv::waitKey(1) == 'q')
+			break;
+	}
+
+	cap.release();
+	cv::destroyAllWindows();
 }
--- camcorder-observer/main.cpp
+++ camcorder-observer/main.cpp
@@ -1,59 +1,22 @@
-#include <opencv2/opencv.hpp>
+#include "TelegramBotConfigurator.hpp"
+#include "TelegramBot.hpp"
+#include "GetBotToken.hpp"
+#include "FaceObserver.hpp"
 
-using namespace cv;
-using namespace std;
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+    tg_bot::TelegramBot bot(config_tool::getTelegramToken("telegram_bot_config.cfg"));
+    std::thread([&] {
+        bot.Start();
+        });
+    FaceObserver observer(bot);
+    std::thread([&] {
+        observer.run();
+        });
 
-class FaceDetector {
-private:
-    CascadeClassifier face_cascade;
-    VideoCapture cap;
+    TelegramBotConfigurator botConfigurator;
+    botConfigurator.show();
 
-public:
-    FaceDetector() {
-        face_cascade.load("haarcascade_frontalface_default.xml");
-        cap.open(0);
-    }
-
-    void detectAndSaveFace() {
-        Mat frame;
-        cap >> frame;
-
-        Mat gray;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-
-        vector<Rect> faces;
-        face_cascade.detectMultiScale(gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-        for (size_t i = 0; i < faces.size(); i++) {
-            Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-            ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4);
-
-            Mat face_roi = frame(faces[i]);
-            imwrite("detected_face.png", face_roi);
-            cout << "Face detected and saved to 'detected_face.png'" << endl;
-        }
-
-        imshow("Face Detection", frame);
-    }
-
-    void run() {
-        namedWindow("Face Detection", WINDOW_AUTOSIZE);
-
-        while (true) {
-            detectAndSaveFace();
-
-            if (waitKey(1) == 'q')
-                break;
-        }
-
-        cap.release();
-        destroyAllWindows();
-    }
-};
-
-int main() {
-    FaceDetector faceDetector;
-    faceDetector.run();
-
-    return 0;
+    return app.exec();
 }
